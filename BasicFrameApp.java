090e830b6e305d3aebb2b7631fc6a07968f146a8
fundiswa20 <139716965+fundiswa20@users.noreply.github.com>
2023-07-17 00:01:33 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,20 @@
+
+package basicframeapp;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+
+public class BasicFrameApp {
+
+
+    public static void main(String[] args) {
+      
+        
+        //declare variable
+        MyFirstFrame myFrame;
+        
+        //instantiate frame
+        myFrame = new MyFirstFrame();
+    }
+    
+}
